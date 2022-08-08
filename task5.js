c547524e3a9853b7825ac936ce560978bd6a6ffd
Alla-Goptsii <goptsy.a@gmail.com>
2022-08-08 12:28:04 +0300

    change structure

--- task5.js
+++
@@ -1,8 +0,0 @@
-const inputEl = document.querySelector("#name-input");
-const inputTextEl = document.querySelector("#name-output");
-console.log(inputEl);
-
-inputEl.addEventListener("input", (event) => {
-  inputTextEl.textContent = event.currentTarget.value;
-  console.log(inputTextEl);
-});
